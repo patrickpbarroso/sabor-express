a3b156f3ab8eb219fbc2b4d8a67c1b7761fd655c
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-12-23 18:28:58 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,79 @@
+import os
+
+restaurantes = ['House Burguer', 'Mandalla Foods']
+
+def exibir_nome_do_programa():
+    print("""
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    """)
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante'
+        + '\n2. Listar restaurantes'
+        + '\n3. Ativar restaurantes'
+        + '\n4. Sair')
+
+def finalizar_app():
+    exibir_subtitulo('...encerrando app')
+
+def opcao_invalida():
+    print('Opção inválida.\n')
+    voltar_ao_menu_principal()
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal ')
+    main()
+
+def exibir_subtitulo(subtitulo):
+    os.system('clear')
+    print(subtitulo)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso.')
+
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Lista de restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('\nEscolha uma opção: '))
+
+        match opcao_escolhida:
+            case 1:
+                cadastrar_novo_restaurante()
+            case 2:
+                listar_restaurantes()
+            case 3:
+                print("Ativar restaurante")
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    os.system('clear')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
